22013cba3e783675c5a58a512c1f610172f8fc06
Manish Arora <manish.arora4926@gmail.com>
2021-06-28 20:18:10 +0530

    pepsnap initiated

---
+++ project/pepsnap/gallery.js
@@ -0,0 +1,5 @@
+function appendPhoto(mediaObj) {
+    let mediaDiv = document.createElement("div");
+    mediaDiv.classList.add("media-div");
+    mediaDiv.innerHTML = `<img class="media-img" src=`;
+}
